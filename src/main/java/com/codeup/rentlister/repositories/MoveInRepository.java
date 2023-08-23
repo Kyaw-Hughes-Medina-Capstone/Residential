e165aef6a2c7527cace105c008cb92bd07c95de1
khinememe kyaw <khinememe.kyaw01@gmail.com>
2023-08-23 10:11:12 -0500

    Completed move-in-checklist

---
+++ src/main/java/com/codeup/rentlister/repositories/MoveInRepository.java
@@ -0,0 +1,10 @@
+package com.codeup.rentlister.repositories;
+
+import com.codeup.rentlister.models.MoveInForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MoveInRepository extends JpaRepository <MoveInForm, Long> {
+    MoveInForm findMoveInFormById(long id);
+}
