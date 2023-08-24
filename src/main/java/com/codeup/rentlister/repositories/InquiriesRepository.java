1bcb622b2c8cc18e047788ca128955751b329ee0
Xavier G Medina <130258237+xavimedi@users.noreply.github.com>
2023-08-24 16:45:32 -0500

    Delete src directory
    test123

--- src/main/java/com/codeup/rentlister/repositories/InquiriesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeup.rentlister.repositories;
-import com.codeup.rentlister.models.Inquiries;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InquiriesRepository extends JpaRepository<Inquiries, Integer> {
-
-	Inquiries findInquiryById(int id);
-}
--- src/main/java/com/codeup/rentlister/repositories/WorkOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeup.rentlister.repositories;
-import com.codeup.rentlister.models.Property;
-import com.codeup.rentlister.models.User;
-import com.codeup.rentlister.models.WorkOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkOrderRepository extends JpaRepository<WorkOrder, Integer> {
-	WorkOrder findWorkOrderById(int id);
-}
