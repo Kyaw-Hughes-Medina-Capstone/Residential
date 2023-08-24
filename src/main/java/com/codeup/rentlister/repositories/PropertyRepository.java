ac0534ff53352c0b91c3a2d3ce59b7a47b717481
Xavier G Medina <130258237+xavimedi@users.noreply.github.com>
2023-08-24 17:21:22 -0500

    end of week

---
+++ src/main/java/com/codeup/rentlister/repositories/PropertyRepository.java
@@ -0,0 +1,13 @@
+package com.codeup.rentlister.repositories;
+import com.codeup.rentlister.models.Property;
+import com.codeup.rentlister.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyRepository extends JpaRepository<Property, Integer> {
+
+	Property findPropertyById(int id);
+	Property findPropertyByAddress(String address);
+	Property findPropertyByZip(int zip);
+	Property findPropertyByTenant(User tenant);
+	Property findPropertyByTenantId(int userId);
+}
---
+++ src/main/java/com/codeup/rentlister/repositories/WorkOrderRepository.java
@@ -0,0 +1,9 @@
+package com.codeup.rentlister.repositories;
+import com.codeup.rentlister.models.Property;
+import com.codeup.rentlister.models.User;
+import com.codeup.rentlister.models.WorkOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkOrderRepository extends JpaRepository<WorkOrder, Integer> {
+	WorkOrder findWorkOrderById(int id);
+}
