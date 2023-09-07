53cceb46aabfd1f4ee9c7daf3e31787568fd9ff7
Xavier G Medina <130258237+xavimedi@users.noreply.github.com>
2023-09-07 09:40:07 -0500

    thu Sep 7

--- src/main/java/com/codeup/rentlister/security/SecurityConfiguration.java
+++ src/main/java/com/codeup/rentlister/security/SecurityConfiguration.java
@@ -44,17 +44,17 @@ public class SecurityConfiguration {
 
                          * anyone can visit the home page, register, login, and view ads */
 
-                        .requestMatchers("/", "/property", "/property/*","/about", "/sign-up", "/login", "/owner/portfolio", "/property/*/detail").permitAll()
+//                        .requestMatchers("/", "/property", "/property/*","/about", "/sign-up", "/login", "/owner/portfolio", "/property/*/detail").permitAll()
 
                    
 
 
 //                          anyone can visit the home page, register, login, and view
-                        .requestMatchers("/", "/property", "/property/*", "/sign-up", "/login", "/about", "/home", "/landing.mov", "/chip.png", "/property/*/detail").permitAll()
+//                        .requestMatchers("/", "/property", "/property/*", "/sign-up", "/login", "/about", "/home", "/landing.mov", "/chip.png", "/property/*/detail").permitAll()
 
 
 //                          anyone can visit the home page, register, login, and view
-                        .requestMatchers("/", "/property", "/property/*", "/sign-up", "/login", "/about", "/home", "/tenant/*", "/contact","/filtered-properties", "/property/*/detail").permitAll()
+                        .requestMatchers("/", "/property", "/property/*", "/sign-up", "/login", "/about", "/home", "/tenant/*", "/contact","/filtered-properties", "/property/*/detail" ).permitAll()
 
                         // allow loading of static resources
                                 .requestMatchers("/css/**", "/js/**", "/img/**", "/pdf/**", "/landing.mov", "/residentialLease.pdf").permitAll()
