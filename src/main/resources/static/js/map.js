1b3ca72c57f4e832f729cd57660ae2c340ef5623
Xavier G Medina <130258237+xavimedi@users.noreply.github.com>
2023-09-12 12:53:45 -0500

    working on property index2

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -8,19 +8,15 @@ const map = new mapboxgl.Map({
     zoom: 8
 });
 
-
-// Array to store marker instances
 const markers = [];
 
-// Perform geocoding and create markers for each marker element
+
 document.querySelectorAll('.card-address').forEach(cardAddress => {
     const address = cardAddress.textContent; // Get the address text
     console.log(address);
     updateMap(address);
 });
 
-
-// Perform geocoding to get coordinates and update the map
 function updateMap(address) {
 
     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapBoxKey}`)
